6905b4e347cac5e9428bbbfd591c4a6b0fb48e6d
Juan Rodríguez <juan.rodriguez_bermudez@etu.uca.fr>
2019-05-18 02:39:30 +0200

    - Created solution - Created initial folder structure for project - Added icons and splash screens for Android and iOS - Added base ViewModel classes - Modified UML diagram according to the new project names

---
+++ Libraries/Randomizer.Framework/Randomizer.Framework.ViewModels/HomePage/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Randomizer.Framework.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+    }
+}
