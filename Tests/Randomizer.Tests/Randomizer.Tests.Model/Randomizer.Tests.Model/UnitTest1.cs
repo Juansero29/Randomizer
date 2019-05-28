ab013f330ab4713fd8af1bc0da78967bbb592e13
Gerben de Vries <gerbencdg@gmail.com>
2019-05-28 13:50:01 +0200

    Created folder hierarchy for tests

---
+++ Tests/Randomizer.Tests/Randomizer.Tests.Model/Randomizer.Tests.Model/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Randomizer.Tests.Model
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 4 + 2);
+        }
+        
+
+    }
+}
