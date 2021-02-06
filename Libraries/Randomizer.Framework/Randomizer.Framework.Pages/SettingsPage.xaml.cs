2f4a15861049bbd3a51cc7abe8cd92a5c3e38067
Juan Rodríguez <juansero29@gmail.com>
2021-02-06 10:31:44 +0100

    Themes are now working

--- Libraries/Randomizer.Framework/Randomizer.Framework.Pages/SettingsPage.xaml.cs
+++ Libraries/Randomizer.Framework/Randomizer.Framework.Pages/SettingsPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using EnigmatiKreations.Framework.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace Randomizer.Pages
@@ -24,25 +26,26 @@ namespace Randomizer.Pages
             Checkbox.IsEnabled = false;
             var originalBounds = Circle.Bounds;
             var originalScale = Circle.Scale;
-            var color = default(Color);
             var brush = default(Brush);
+
+            var themeMergedDictionary = Application.Current.Resources.MergedDictionaries.Where(d => d.MergedDictionaries.Count == 1).FirstOrDefault();
+            if (themeMergedDictionary != null) themeMergedDictionary.MergedDictionaries.Clear();
+
             if (e.Value)
             {
-                Application.Current.UserAppTheme = OSAppTheme.Dark;
+                Application.Current.UserAppTheme = OSAppTheme.Light;
+                themeMergedDictionary.MergedDictionaries.Add(new LightTheme());
+                brush = Brush.White;
 
-                color = (Color)Application.Current.Resources["DarkPrimaryColor"];
-                brush = Brush.Black;
             }
             else
             {
-                Application.Current.UserAppTheme = OSAppTheme.Light;
-
-                color = (Color)Application.Current.Resources["LightPrimaryColor"];
-                brush = Brush.White;
+                Application.Current.UserAppTheme = OSAppTheme.Dark;
+                themeMergedDictionary.MergedDictionaries.Add(new DarkTheme());
+                brush = Brush.Black;
             }
 
             Circle.IsVisible = true;
-
             var rect = new Rectangle(0, 0, 1000, 1000);
             Circle.Fill = brush;
             await Circle.LayoutTo(rect, length: _animationSpeed, easing: Easing.SinInOut);
